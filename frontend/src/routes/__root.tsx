e90916303bfadfc789a004ce46426e963a55eb78
atharva <atharva.sugandhi@gmail.com>
2025-10-30 00:49:09 +0530

    [FRONTEND] Initialise VITE + REACT(ts)

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,24 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+
+      {import.meta.env.DEV &&
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />}
+    </>
+  ),
+})
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          '"Fira Sans"',
+          '"Droid Sans"',
+          '"Helvetica Neue"',
+          'sans-serif',
+        ],
+        mono: [
+          'source-code-pro',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Courier New"',
+          'monospace',
+        ],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
